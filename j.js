889f224f6d25e6efed69cd086e699d03aa0ae3a5
mohib <bekaltimohiboss@gmail.com>
2022-11-10 19:13:27 +0100

    youtube app video serch

---
+++ j.js
@@ -0,0 +1,15 @@
+var action="click";
+var speed="500";
+
+$(document).ready(function(){
+    $('li.q').on('click',function(){
+        $(this).next()
+        .slideToggle(speed)
+            .siblings('li.a')
+                .slideUp();
+        var img = $(this).children('img');
+        $('img').not(img).removeClass('rotate');
+        img.toggleClass('rotate')   
+
+    });
+});
